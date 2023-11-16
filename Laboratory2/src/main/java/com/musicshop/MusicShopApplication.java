eb1e6940218fbe741a3394b5bda15525ddae7160
Chris Joyce <56563947+Jayce132@users.noreply.github.com>
2023-11-17 01:50:03 +0200

    Added ApplicationContext that uses Factory and Singleton pattern

--- Laboratory2/src/main/java/com/musicshop/MusicShopApplication.java
+++ Laboratory2/src/main/java/com/musicshop/MusicShopApplication.java
@@ -1,51 +1,19 @@
 package com.musicshop;
 
-import com.musicshop.controller.cart.CartController;
-import com.musicshop.controller.product.ProductController;
-import com.musicshop.model.cart.Cart;
-import com.musicshop.model.product.Product;
-import com.musicshop.repository.cart.CartDetailRepository;
-import com.musicshop.repository.cart.CartRepository;
-import com.musicshop.repository.product.ProductRepository;
+import com.musicshop.config.ApplicationContext;
+
 import com.musicshop.ui.MusicShopConsole;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class MusicShopApplication {
 
     public static void main(String[] args) {
-        ProductRepository productRepository = new ProductRepository();
-        CartRepository cartRepository = new CartRepository();
-        CartDetailRepository cartDetailRepository = new CartDetailRepository();
+        ApplicationContext context = ApplicationContext.getInstance();
 
-        // Creating and adding products
-        Product product1 = new Product();
-        product1.setName("Martin LX1E");
-        product1.setDescription("Portable and sounds great");
-        product1.setPrice(new BigDecimal("550.99"));
-        product1.setQuantityAvailable(10);
-        product1.setCategoryID(2L);
-        productRepository.save(product1);
-
-        Product product2 = new Product();
-        product2.setName("Fender Player Stratocaster");
-        product2.setDescription("Classic sound and look");
-        product2.setPrice(new BigDecimal("649.99"));
-        product2.setQuantityAvailable(8);
-        product2.setCategoryID(3L);
-        productRepository.save(product2);
-
-        // Creating and adding carts
-        Cart cart1 = new Cart();
-        cart1.setCustomerID(2L);
-        cart1.setDateCreated(LocalDateTime.parse("2023-10-15T00:00:00"));
-        cartRepository.save(cart1);
-
-        ProductController productController = new ProductController(productRepository);
-        CartController cartController = new CartController(cartRepository, cartDetailRepository);
-
-        MusicShopConsole console = new MusicShopConsole(productController, cartController);
+        MusicShopConsole console = new MusicShopConsole(
+                context.getProductController(),
+                context.getCartController()
+        );
         console.start();
     }
 }
